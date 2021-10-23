3acc6e800f62b32a92bb2bd62b1ca1726f20f9f6
UBCh <u2b127@gmail.ru>
2021-10-23 13:08:57 +0300

    edit

--- src/main/java/SQRService.java
+++ src/main/java/SQRService.java
@@ -6,13 +6,12 @@ public class SQRService {
 
         for (int i = 10; i < 100; i++) {
             if (i * i > bottomMinimum && i * i < bottomMaximum) {
-                System.out.println(" квадрат " + i + " = " + i * i + " это ,больше " + bottomMinimum + " это меньше " + bottomMaximum);
+
                 zero++;
                 if (i * i > bottomMaximum) {
                     break;
                 }
-                System.out.println("возвращаемое значение" + zero);
-            }
+                            }
         } return zero;
     }
 
